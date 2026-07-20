0cf10862efa41fa7e95890c9e46f9ca78307bb14
soblishch <sofiblishch@gmail.com>
2026-07-20 17:03:12 +0300

    update README.md

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 ticker = "KER.WA"
 
 data = yf.download(ticker, start='2025-07-19', end='2026-07-19', interval="1d")
+
+
 data.columns = data.columns.droplevel(1)
 
 data = data.dropna()
