c30224130f8f85e76ca0cbe1a9f31e5e8d673e82
olha-kagramanyan <olha.kahramanian.25@kse.org.ua>
2026-07-21 17:33:11 +0300

    Calculated moving average and added cycle that gives signal 1 if moving average for 20 days is higher than moving average for 50 days.

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ data.columns = data.columns.droplevel(1)
 
 data = data.dropna()
 
+# зміна з 0 на 1, то ми купуємо акцію
+# якщо є послідовність з 1, то ми тримаємо акцію
+# якщо зміна з 1 на 0, то ми продаємо акцію
+
+short_ma = 20
+long_ma = 50
+
+data["Moving average - 20"] = data["Close"].rolling(short_ma).mean()
+data["Moving average - 50"] = data["Close"].rolling(long_ma).mean()
+
+data["Signal"] = 0
+for date in data.index:
+    if data.loc[date, "Moving average - 20"] > data.loc[date, "Moving average - 50"]:
+        data.loc[date, "Signal"] = 1
+
 print(data)
